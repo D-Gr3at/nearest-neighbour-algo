83f9fb76304ea4a71437ebbca477709685821228
D-Gr3at <billeibinabo@gmail.com>
2021-11-06 01:58:44 +0100

    all-1

--- assets/include/js.php
+++
@@ -1,7 +0,0 @@
-<script src="assets/js/jquery-3.6.0.min.js"></script>
-<script src="assets/js/all.min.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/js/main.js"></script>
-<script src="assets/js/toastr.min.js"></script>
-
-<?php
--- assets/lib/database.php
+++ assets/lib/database.php
@@ -1,27 +1,15 @@
 <?php
 
-namespace Database;
-
-require_once 'vendor/autoload.php';
-
-use Dotenv\Dotenv;
-use mysqli;
-
-$dotEnv = Dotenv::create('.');
-
-$dotEnv->load();
-
-$dotEnv->required(['DB_NAME', 'DB_USER'])->notEmpty();
-
 class Database{
 
-    public static function getConnectionInstance(): mysqli
-    {
-        $hostName = getenv('DB_HOST');
-        $databaseName = getenv('DB_NAME');
-        $username = getenv('DB_USER');
-        $password = getenv('DB_PASSWORD');
-        return new mysqli($hostName, $username, $password, $databaseName);
+    private $username;
+    private $password;
+    private $hostName;
+    private $databaseName;
+
+    public function getConnectionInstance(){
+        return new mysqli($this->hostName, $this->username, $this->password, $this->databaseName);
     }
+
 
 }
